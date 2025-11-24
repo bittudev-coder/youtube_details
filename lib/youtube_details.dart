64623250acb264e54164411feb30293d2d7e8363
Bittu Coder <169143833+bittudev-coder@users.noreply.github.com>
2025-11-24 19:40:45 +0530

    Add exports for channel and video models

--- lib/youtube_details.dart
+++ lib/youtube_details.dart
@@ -4,5 +4,11 @@
 library;
 
 export 'src/youtube_details_base.dart';
+export 'src/youtube_details_base.dart';
+export 'src/Model/channel_details_model.dart';
+export 'src/Model/posts_model.dart';
+export 'src/Model/shorts_model.dart';
+export 'src/Model/video_model.dart';
+export 'src/Model/videos_model.dart';
 
 // TODO: Export any libraries intended for clients of this package.
